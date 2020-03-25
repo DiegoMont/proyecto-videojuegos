1780104f4c4ab52ad63d6438a31e70f9138b232e
Ángel Heredia v <48765752+angl6199@users.noreply.github.com>
2020-03-24 23:14:28 -0600

    Add files via upload

---
+++ Proyecto_Videojuegos/Assets/Scripts/Position.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    public Vector3 position;
+    public CarController car;
+    public bool occuppied;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = gameObject.transform.position;
+        occuppied = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (car != null)
+        {
+            if (car.isfixed == 1)
+            {
+                occuppied = false;
+                car = null;
+            }
+        }
+    }
+}
---
+++ Proyecto_Videojuegos/Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SpawnerController : MonoBehaviour
+{
+    public GameObject carToSpawn;
+    public Position[] positions;
+    public CarController carcontrol;
+    private int randomindex = 0;
+    public GameManagerController managerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnCar(){
+        float randomfloat = UnityEngine.Random.Range(0, positions.Length);
+        randomindex = (int) Math.Round(randomfloat);
+        if (positions[randomindex].occuppied == false && carToSpawn != null)
+            {
+                Instantiate(carToSpawn, positions[randomindex].position, Quaternion.identity);
+                positions[randomindex].occuppied = true;
+                managerController.totalcars ++;
+            }
+        else
+            {
+                SpawnCar();
+            }
+    }
+
+    public void SetCarToPosition(){
+        positions[randomindex].car = carcontrol;
+        Debug.Log("Spawner envia a position");
+    }
+}
