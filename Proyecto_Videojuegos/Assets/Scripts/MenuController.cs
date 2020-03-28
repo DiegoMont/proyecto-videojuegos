1b3e29fe4969ecc22613365367313ef6483dbfa9
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2020-03-27 23:25:13 -0600

    MENU DEL JUEGO

---
+++ Proyecto_Videojuegos/Assets/Scripts/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+    public static string dificultad;
+
+    public void DificultadFacil() {
+        dificultad = "facil";
+    }
+
+    public void DificultadMedia() {
+        dificultad = "media";
+    }
+
+    public void DificultadDificil() {
+        dificultad = "dificil";
+    }
+
+    private bool comprobarDificultad() {
+        return dificultad != null && (dificultad == "facil" || dificultad == "media" || dificultad == "dificil");
+    }
+
+    public void jugarForeverAlone() {
+        if(comprobarDificultad())
+            Debug.Log("Cambiar a Scene jugar contra la computadora");
+    }
+
+    public void modoCooperativo()
+    {
+        if(comprobarDificultad())
+            Debug.Log("Cambiar a Scene dos jugadores");
+    }
+}
