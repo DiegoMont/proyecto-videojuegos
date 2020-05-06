35dd662f6b2774bff05a21d180b09efab9b42915
Conde <carlosconde@live.com.mx>
2020-05-06 16:01:14 -0500

    Tire throwing implemented

---
+++ Proyecto_Videojuegos/Assets/Scripts/RaceTireSpawnerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceTireSpawnerController : MonoBehaviour
+{
+ 
+	public GameObject tire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnTire() {
+    	Vector3 position = gameObject.transform.position;
+    	Instantiate(tire, position, Quaternion.identity);
+    }
+}
---
+++ Proyecto_Videojuegos/Assets/Scripts/RaceTireTriggerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceTireTriggerController : MonoBehaviour
+{
+    
+
+	public GameObject[] spawners;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawners = GameObject.FindGameObjectsWithTag("RaceTireSpawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("RacePlayer")){
+        	foreach (GameObject spawner in spawners) {
+        		spawner.GetComponent<RaceTireSpawnerController>().spawnTire();
+        	}
+        }
+    }
+}
