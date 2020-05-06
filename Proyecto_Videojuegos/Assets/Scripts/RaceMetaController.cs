5bf9be4e928783486c8dbfad198f78a7a3b08bb5
Conde <carlosconde@live.com.mx>
2020-05-05 19:35:39 -0500

    Attempt change bar

---
+++ Proyecto_Videojuegos/Assets/Scripts/RaceMetaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaceMetaController : MonoBehaviour
+{
+    
+
+	public GameObject lap;
+	public int totalLaps;
+	public int currentLaps;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	//lap.GetComponent<Text>().text = "ok";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+/*
+    public void updateLaps(int currentLaps, int totalLaps) {
+        lapText.text = currentLaps + "/" + totalLaps;
+     }
+     */
+}
