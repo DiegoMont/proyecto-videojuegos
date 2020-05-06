e0ab05ab2afdcb5d2213668cec00ea3b6c65c89d
Conde <carlosconde@live.com.mx>
2020-05-06 17:15:43 -0500

    Ambulance v2

---
+++ Proyecto_Videojuegos/Assets/Scripts/RaceAmbulanceController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceAmbulanceController : MonoBehaviour
+{
+    
+	public Transform target;
+	public Vector3 _target;
+	public float Speed;
+	public GameObject ObjectToRotate;
+    public float DegreesOffset = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
