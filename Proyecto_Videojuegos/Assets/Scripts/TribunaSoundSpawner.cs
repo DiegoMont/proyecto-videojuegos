9552104537b9d1b2cd701af26ee5bd7936cc7b08
Conde <carlosconde@live.com.mx>
2020-05-06 21:59:21 -0500

    Tribuna sounds implemented

---
+++ Proyecto_Videojuegos/Assets/Scripts/TribunaSoundSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TribunaSoundSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+	public GameObject sound;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.gameObject.CompareTag("RacePlayer")){
+            Instantiate(sound, gameObject.transform.position, gameObject.transform.rotation);        }
+    }
+}
